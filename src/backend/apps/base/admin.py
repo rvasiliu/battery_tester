a905d92c804e14196e4d958576a26783d6f0bf06
Mihai Dobre <mihai.dobre@powervault.co.uk>
2018-01-11 15:37:09 +0000

    refactored the get_graph in the admin.py

--- src/backend/apps/base/admin.py
+++ src/backend/apps/base/admin.py
@@ -22,7 +22,7 @@ class InverterPoolAdmin(admin.ModelAdmin):
 
 class TestCaseAdmin(admin.ModelAdmin):
     model = TestCase
-    list_display = ('id', 'name', 'get_battery', 'get_inverter', 'state')
+    list_display = ('id', 'name', 'get_battery', 'get_inverter', 'state', 'get_graph')
     list_filter = ('name',)
 
     def get_battery(self, instance):
@@ -33,33 +33,9 @@ class TestCaseAdmin(admin.ModelAdmin):
         return instance.inverter.name
     get_inverter.short_description = 'Inverter'
 
-    # def get_graph(self, instance):
-    #     timestamps = None
-    #     refresh = ''
-    #     # get timestamp for test start
-    #     try:
-    #         tc = TestResult.objects.filter(test_case=instance.id)
-    #         timestamps = set([int(t.timestamp.strftime("%s")) for t in tc])
-    #     except Exception as err:
-    #         print(err)
-    #     print('################', instance.id)
-    #     if timestamps:
-    #         tc_start_timestamp = min(timestamps)*1000
-    #         tc_stop_timestamp = max(timestamps)*1000
-    #     else:
-    #         tc_start_timestamp = 'now'
-    #         tc_stop_timestamp = 'now'
-    #     print('################', tc_start_timestamp)
-    #     print('################', tc_stop_timestamp)
-    #     if instance.state == 'RUNNING':
-    #         tc_stop_timestamp = 'now'
-    #         refresh = '&refresh=5s'
-    #     url_params = 'var-test_case_id={tc_id}&from={start_timestamp}&to={stop_timestamp}{refresh}'.format(tc_id=instance.id,
-    #                                                                                                        start_timestamp=tc_start_timestamp,
-    #                                                                                                        stop_timestamp=tc_stop_timestamp,
-    #                                                                                                        refresh=refresh)
-    #     return mark_safe('<a href="http://localhost:9000/dashboard/db/cells-voltage?{url_params}">graph</a>'.format(url_params=url_params))
-    # get_graph.short_description = 'Graph'
+    def get_graph(self, instance):
+        return mark_safe('<a href="{graph}">graph</a>'.format(graph=instance.graph))
+    get_graph.short_description = 'Graph'
 
 
 admin.site.register(Battery, BatteryAdmin)
